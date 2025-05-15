521a3a76a2efd04e5ede9809da6c9d9e316c28df
Alexander Polousov <polousov79@gmail.com>
2025-05-16 01:13:25 +0300

    feature добавил

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ for i in "Hello word":
     print(i)
 
 print('___________________________')
-
+def feature():
+    print("feature")
+    
 def main():
     yield from (i for i in range(10) if i%2 == 0)
